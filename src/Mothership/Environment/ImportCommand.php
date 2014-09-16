b72a7002d2ce3c2f4d2b58cd0611da2259ffdddd
Don Bosco van Hoi <vanhoi@mothership.de>
2014-09-16 15:01:32 +0200

    Import fixed

--- src/Mothership/Environment/ImportCommand.php
+++ src/Mothership/Environment/ImportCommand.php
@@ -39,9 +39,10 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Finder\Finder;
+use Mothership\Lib\Database;
 use Mothership\Lib\File;
 
-class ImportCommand extends AbstractMagentoCommand
+class ImportCommand extends Database
 {
     /**
      * @var SimpleXMLElement
@@ -130,18 +131,111 @@ class ImportCommand extends AbstractMagentoCommand
             foreach ($config as $path => $data) {
 
                 foreach ($data as $_config_data) {
-                    $currentValue = \Mage::getStoreConfig($path, $_config_data['scope_id']);
 
-                    //\Mage::app()->getConfig()->saveConfig($values['path'], $values['value'], $values['scope'], $storeId);
+                    $currentValue = $this->_getStoreConfig($_config_data['scope'], $_config_data['scope_id'], $path);
+
+                    $changed = 'no';
+                    if ($currentValue != $_config_data['value']) {
+                        $this->_setStoreConfig($_config_data['scope'], $_config_data['scope_id'], $path, $_config_data['value']);
+                        $changed = 'yes';
+                    }
+
                     $table[] = array (
                         'path'     => $path,
                         'old_value' => substr($currentValue, 0, 40),
-                        'new_value' => substr($_config_data['value'], 0, 40)
+                        'new_value' => substr($_config_data['value'], 0, 40),
+                        'changed'   => $changed,
                     );
                 }
             }
 
             $this->getHelper('table')->write($output, $table);
         };
+    }
+
+    /**
+     * Get the store config based on plain database queries. This is safer then the native magento function
+     *
+     * @param string $scope    the scope, for example default or website
+     * @param int    $scope_id the scope id works similar to the store id
+     * @param string $path     the configuration path in the core_config_data
+     *
+     * @return string
+     */
+    protected function _getStoreConfig($scope, $scope_id, $path)
+    {
+        $dbh = $this->_getDatabaseConnection();
+        $sql = 'SELECT value
+                  FROM core_config_data
+                  WHERE scope = :scope AND scope_id = :scope_id AND path = :path';
+
+        $sth = $dbh->prepare($sql);
+        $sth->execute(array(
+                ':scope'    => $scope,
+                ':scope_id' => $scope_id,
+                ':path'     => $path
+            )
+        );
+        $result = $sth->fetch(\PDO::FETCH_ASSOC);
+        return (isset($result['value'])) ? $result['value'] : '';
+    }
+
+    /**
+     * Set the store config
+     *
+     * @param string $scope    the scope, for example default or website
+     * @param int    $scope_id the scope id works similar to the store id
+     * @param string $path     the configuration path in the core_config_data
+     * @param string $value    the value for a specific combination of scope, scope_id and path
+     *
+     * @return bool
+     */
+    protected function _setStoreConfig($scope, $scope_id, $path, $value)
+    {
+        $dbh = $this->_getDatabaseConnection();
+        $sql = 'SELECT config_id, value
+                  FROM core_config_data
+                  WHERE scope = :scope AND scope_id = :scope_id AND path = :path';
+
+        $sth = $dbh->prepare($sql);
+        $sth->execute(array(
+                ':scope'    => $scope,
+                ':scope_id' => $scope_id,
+                ':path'     => $path
+            )
+        );
+        $result = $sth->fetch(\PDO::FETCH_ASSOC);
+
+        if (isset($result['config_id'])) {
+
+            $dbh = $this->_getDatabaseConnection();
+            $sql = 'UPDATE core_config_data
+                        SET value = :value
+                        WHERE scope = :scope AND scope_id = :scope_id AND path = :path';
+
+            $sth = $dbh->prepare($sql);
+            $sth->execute(array(
+                    ':scope'    => $scope,
+                    ':scope_id' => $scope_id,
+                    ':path'     => $path,
+                    ':value'    => $value
+                )
+            );
+
+        } else {
+            $dbh = $this->_getDatabaseConnection();
+            $sql = 'INSERT INTO core_config_data(scope, scope_id, path, value)
+                  VALUES (:scope, :scope_id, :path, :value)';
+
+            $sth = $dbh->prepare($sql);
+            $sth->execute(array(
+                    ':scope'    => $scope,
+                    ':scope_id' => $scope_id,
+                    ':path'     => $path,
+                    ':value'    => $value
+                )
+            );
+            $sth->exec();
+        }
     }
 }
