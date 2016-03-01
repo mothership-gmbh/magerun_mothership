84522848a77f0a99e165a39e487f873551abc5dc
Don Bosco van Hoi <vanhoi@mothership.de>
2016-03-01 15:11:58 +0100

    fix

--- _deploy/travis.sh
+++ _deploy/travis.sh
@@ -8,8 +8,8 @@ SOURCE_DIR=`dirname "${ABSPATH}"`
 
 if [ -z $MAGENTO_DB_HOST ]; then MAGENTO_DB_HOST="localhost"; fi
 if [ -z $MAGENTO_DB_PORT ]; then MAGENTO_DB_PORT="3306"; fi
-if [ -z $MAGENTO_DB_USER ]; then MAGENTO_DB_USER="test"; fi
-if [ -z $MAGENTO_DB_PASS ]; then MAGENTO_DB_PASS="test"; fi
+if [ -z $MAGENTO_DB_USER ]; then MAGENTO_DB_USER="root"; fi
+if [ -z $MAGENTO_DB_PASS ]; then MAGENTO_DB_PASS=""; fi
 if [ -z $MAGENTO_DB_NAME ]; then MAGENTO_DB_NAME="mageteststand"; fi
 if [ -z $MAGENTO_DB_ALLOWSAME ]; then MAGENTO_DB_ALLOWSAME="0"; fi
 
@@ -50,7 +50,6 @@ if [ ! -f htdocs/app/etc/local.xml ] ; then
     if [ ! -z $MAGENTO_DB_PASS ]; then MYSQLPASS="-p${MAGENTO_DB_PASS}"; fi
     mysql -u${MAGENTO_DB_USER} ${MYSQLPASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}\`; CREATE DATABASE \`${MAGENTO_DB_NAME}\`;"
 
-
     ./n98-magerun-latest.phar install \
       --dbHost="${MAGENTO_DB_HOST}" --dbUser="${MAGENTO_DB_USER}" --dbPass="${MAGENTO_DB_PASS}" --dbName="${MAGENTO_DB_NAME}" --dbPort="${MAGENTO_DB_PORT}" \
       --installSampleData=yes \
@@ -62,7 +61,7 @@ if [ ! -f htdocs/app/etc/local.xml ] ; then
      mysql -u${MAGENTO_DB_USER} -p${MAGENTO_DB_PASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}\`; CREATE DATABASE \`${MAGENTO_DB_NAME}\`;"
 fi
 
-composer.phar self-update
+composer install
 
 
 modman deploy-all --force
