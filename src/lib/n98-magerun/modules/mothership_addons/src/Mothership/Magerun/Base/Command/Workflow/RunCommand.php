bba7132efee4c33dd02248e737e7e7c2e51d9588
Don Bosco van Hoi <vanhoi@mothership.de>
2016-03-01 18:52:16 +0100

    matched a command so that it is now compatible with the interface

--- src/lib/n98-magerun/modules/mothership_addons/src/Mothership/Magerun/Base/Command/Workflow/RunCommand.php
+++ src/lib/n98-magerun/modules/mothership_addons/src/Mothership/Magerun/Base/Command/Workflow/RunCommand.php
@@ -136,7 +136,7 @@ HELP;
      * @param \Symfony\Component\Console\Input\InputInterface   $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      */
-    protected function interact($input, $output)
+    protected function interact(InputInterface $input, OutputInterface $output)
     {
         if ($input->hasOption('interactive') && $input->getOption('interactive')) {
 
