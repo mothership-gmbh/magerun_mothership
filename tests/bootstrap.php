4577241e606f5e0f45ca9849bdb94313d5ad894c
Don Bosco van Hoi <vanhoi@mothership.de>
2016-03-01 16:53:09 +0100

    Changed paths so that tests will run from cwd now

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,21 @@
 
 <?php
 $base = getenv('N98_MAGERUN_TEST_MAGENTO_ROOT');
-if (FALSE === $base) {
+if (false === $base) {
     unset($base);
+
     return;
 }
 @session_start();
-$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader = require getcwd() . '/../vendor/autoload.php';
+
 /* @var $loader \Composer\Autoload\ClassLoader */
 $loader->setUseIncludePath(true);
 $paths = array(
-    $base . '/app/code/local',
-    $base . '/app/code/community',
-    $base . '/app/code/core',
-    $base . '/lib',
+    $base.'/app/code/local',
+    $base.'/app/code/community',
+    $base.'/app/code/core',
+    $base.'/lib',
 );
-set_include_path(implode(PATH_SEPARATOR, $paths) . PATH_SEPARATOR . get_include_path());
+set_include_path(implode(PATH_SEPARATOR, $paths).PATH_SEPARATOR.get_include_path());
 unset($paths, $base);
