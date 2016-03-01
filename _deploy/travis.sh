5e589c2010709f51e1901ec6db0cf0bfe4ea4e08
Don Bosco van Hoi <vanhoi@mothership.de>
2016-03-01 14:24:48 +0100

    Added custom travis script

---
+++ _deploy/travis.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Get absolute path to main directory
+ABSPATH=$(cd "${0%/*}" 2>/dev/null; echo "${PWD}/${0##*/}")
+SOURCE_DIR=`dirname "${ABSPATH}"`
+
+if [ -z $MAGENTO_DB_HOST ]; then MAGENTO_DB_HOST="localhost"; fi
+if [ -z $MAGENTO_DB_PORT ]; then MAGENTO_DB_PORT="3306"; fi
+if [ -z $MAGENTO_DB_USER ]; then MAGENTO_DB_USER="root"; fi
+if [ -z $MAGENTO_DB_PASS ]; then MAGENTO_DB_PASS=""; fi
+if [ -z $MAGENTO_DB_NAME ]; then MAGENTO_DB_NAME="mageteststand"; fi
+if [ -z $MAGENTO_DB_ALLOWSAME ]; then MAGENTO_DB_ALLOWSAME="0"; fi
+
+echo
+echo "---------------------"
+echo "- AOE MageTestStand -"
+echo "---------------------"
+echo
+echo "Installing ${MAGENTO_VERSION} in ${SOURCE_DIR}/htdocs"
+echo "using Database Credentials:"
+echo "    Host: ${MAGENTO_DB_HOST}"
+echo "    Port: ${MAGENTO_DB_PORT}"
+echo "    User: ${MAGENTO_DB_USER}"
+echo "    Pass: [hidden]"
+echo "    Main DB: ${MAGENTO_DB_NAME}"
+echo "    Test DB: ${MAGENTO_DB_NAME}_test"
+echo "    Allow same db: ${MAGENTO_DB_ALLOWSAME}"
+echo
+
+
+
+
+cd ${SOURCE_DIR}
+
+# Download composer
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php composer-setup.php
+php composer.phar self-update
+bash < <(curl -s -L https://raw.github.com/colinmollenhour/modman/master/modman-installer)
+wget http://files.magerun.net/n98-magerun-latest.phar
+chmod +x ./n98-magerun-latest.phar
+
+
+if [ ! -f htdocs/app/etc/local.xml ] ; then
+
+    # Create main database
+    MYSQLPASS=""
+    if [ ! -z $MAGENTO_DB_PASS ]; then MYSQLPASS="-p${MAGENTO_DB_PASS}"; fi
+    mysql -u${MAGENTO_DB_USER} ${MYSQLPASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}\`; CREATE DATABASE \`${MAGENTO_DB_NAME}\`;"
+
+    sed -i -e s/MAGENTO_DB_HOST/${MAGENTO_DB_HOST}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+    sed -i -e s/MAGENTO_DB_PORT/${MAGENTO_DB_PORT}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+    sed -i -e s/MAGENTO_DB_USER/${MAGENTO_DB_USER}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+    sed -i -e s/MAGENTO_DB_PASS/${MAGENTO_DB_PASS}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+    sed -i -e s/MAGENTO_DB_ALLOWSAME/${MAGENTO_DB_ALLOWSAME}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+
+    if [ $MAGENTO_DB_ALLOWSAME == "0" ] ; then
+      # Create test database
+      mysql -u${MAGENTO_DB_USER} ${MYSQLPASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}_test\`; CREATE DATABASE \`${MAGENTO_DB_NAME}_test\`;"
+      sed -i -e s/MAGENTO_DB_NAME/${MAGENTO_DB_NAME}_test/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+    else
+      sed -i -e s/MAGENTO_DB_NAME/${MAGENTO_DB_NAME}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+    fi
+
+    tools/n98-magerun.phar install \
+      --dbHost="${MAGENTO_DB_HOST}" --dbUser="${MAGENTO_DB_USER}" --dbPass="${MAGENTO_DB_PASS}" --dbName="${MAGENTO_DB_NAME}" --dbPort="${MAGENTO_DB_PORT}" \
+      --installSampleData=yes \
+      --useDefaultConfigParams=yes \
+      --magentoVersionByName="${MAGENTO_VERSION}" \
+      --installationFolder="${SOURCE_DIR}/htdocs" \
+      --baseUrl="http://magento.local/" || { echo "Installing Magento failed"; exit 1; }
+
+     mysql -u${MAGENTO_DB_USER} -p${MAGENTO_DB_PASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}\`; CREATE DATABASE \`${MAGENTO_DB_NAME}\`;"
+fi
+
+composer.phar self-update
+
+
+modman deploy-all --force
+
+n98-magerun-latest.phar --root-dir=htdocs config:set dev/template/allow_symlink 1
+n98-magerun-latest.phar --root-dir=htdocs sys:setup:run
+n98-magerun-latest.phar --root-dir=htdocs cache:flush
+
+n98-magerun-latest.phar cache:clean
+n98-magerun-latest.phar sys:module:list
+export N98_MAGERUN_TEST_MAGENTO_ROOT=${BUILDENV}/htdocs
+
+
+cd ${BUILDENV}/htdocs
+phpunit --debug --verbose
