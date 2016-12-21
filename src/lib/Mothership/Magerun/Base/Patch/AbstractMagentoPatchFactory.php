16cc257aa1a9a4252a3c0de785e55424274a8f17
Don Bosco van Hoi <vanhoi@mothership.de>
2016-12-21 14:41:03 +0100

    Refactored the directory structure

--- src/lib/Mothership/Magerun/Base/Patch/AbstractMagentoPatchFactory.php
+++ src/lib/Mothership/Magerun/Base/Patch/AbstractMagentoPatchFactory.php
@@ -2,10 +2,10 @@
 /**
  * This file is part of the Mothership GmbH code.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
  */
-namespace Mothership\Magerun\Patch;
+namespace Mothership\Magerun\Base\Patch;
 /**
  * Class AbstractMagentoPatchFactory
  *
